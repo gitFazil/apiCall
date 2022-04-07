01358fc7fe086c8330d2f142b5890e42acc8983f
Mohd Fazil <kfazil185@gmail.com>
2022-04-07 12:34:01 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    async function api() {
+      await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json().then(data => setList(data))).catch(err => err)
+    }
+    api();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map(data => <p key={data.id}>{data.name}</p>)}
     </div>
   );
 }
